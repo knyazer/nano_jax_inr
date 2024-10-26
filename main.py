a030dc8a1602e709ae08f5f3a898e77e1540c09e
Roman Knyazhitskiy <git@knyaz.tech>
2024-10-26 14:06:03 +0200

    feat: integrate latent mapping and training flow

--- main.py
+++ main.py
@@ -6,73 +6,106 @@ import equinox as eqx
 import jax
 import jax.numpy as jnp
 import jax.random as jr
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import optax
 from absl import app, logging
-from jaxtyping import Array, Float, PRNGKeyArray, PyTree
+from jax.scipy.signal import convolve
+from jaxtyping import Array, Float, Int, PRNGKeyArray, PyTree
 from PIL import Image
 from sklearn.datasets import fetch_openml
 
 from dataloader import load_imagenette_images, prepare_datasets, preprocess_mnist
 
+mpl.use("TkAgg")
+
+
+def make_mesh(shape):
+    return jnp.stack(jnp.meshgrid(jnp.arange(shape[0]), jnp.arange(shape[1]))).reshape(2, -1).T
+
+
+class LatentMap(eqx.Module):
+    """Parametrized function to convert point in 2D (pixel coordinate) into a latent."""
+
+    positions: Int[Array, "n dim"]
+    neighbor_map: Int[Array, "w h num_neighbors"]
+    embeddings: Array
+
+    def __init__(self, key, positions, shape=(28, 28)):
+        positions = jnp.array(positions)
+
+        def compute_nearest_indices(request_pos):
+            distances = jnp.linalg.norm(jnp.array(positions) - request_pos, axis=1)
+            return jnp.argsort(distances)[:4]
+
+        coords = make_mesh(shape)
+        nearest_indices = jax.vmap(compute_nearest_indices)(coords)
+        neighbor_map = jnp.zeros((shape[0], shape[1], 4))
+        neighbor_map = neighbor_map.at[coords[:, 0], coords[:, 1]].set(nearest_indices)
+
+        self.neighbor_map = neighbor_map.astype(jnp.int16)
+        self.positions = positions
+        self.embeddings = jr.normal(key, (len(positions), 32)) * 0.1
+
+    def __call__(self, position: Float[Array, "2"], *, ensure_valid=True):
+        int_pos = jnp.floor(position).astype(jnp.int32)
+        # linear-ish interpolation of neighbors
+        if ensure_valid:
+            int_pos = eqx.error_if(
+                int_pos,
+                jnp.any(
+                    jnp.logical_or(int_pos < 0, int_pos >= jnp.array(self.neighbor_map.shape[:2]))
+                ),
+                f"Requested position is out of bounds: {self.neighbor_map.shape[:2]}",
+            )
+
+        neighbors = self.neighbor_map[int_pos[0], int_pos[1]]
+        distances = jnp.linalg.norm(self.positions[neighbors] - int_pos, axis=1)
+        weights = 1.0 / (distances + 1e-8)
+
+        latent = jnp.sum(weights[:, None] * self.embeddings[neighbors], axis=0) / jnp.sum(weights)
+        return jax.lax.cond(weights.sum() > 1e3, lambda: latent, lambda: jnp.zeros_like(latent))
+
+
+"""
+latent_map = LatentMap(jr.PRNGKey(0), [(1, 1), (3, 3), (1, 3), (3, 1), (0, 0)], (4, 4))
+assert jnp.allclose(latent_map(jnp.array([0, 0], dtype=jnp.int32)), latent_map.embeddings[4])
+assert jnp.allclose(latent_map(jnp.array([1, 1], dtype=jnp.int32)), latent_map.embeddings[0])
+assert jnp.allclose(
+    latent_map(jnp.array([2, 2], dtype=jnp.int32)),
+    jnp.mean(latent_map.embeddings[:-1], axis=0),
+)
+del latent_map
+"""
+
 
 class MLP(eqx.Module):
     layers: list[eqx.nn.Linear]
-    scale: float = eqx.field(static=True)
 
-    def __init__(
-        self,
-        in_dim: int,
-        hidden_dims: list[int],
-        out_dim: int,
-        key: PRNGKeyArray,
-        scale: float = 1e-2,
-    ):
+    def __init__(self, in_dim: int, hidden_dims: list[int], out_dim: int, key: PRNGKeyArray):
         keys = jr.split(key, len(hidden_dims) + 1)
         self.layers = [
             eqx.nn.Linear(in_dim if i == 0 else hidden_dims[i - 1], hidden_dims[i], key=keys[i])
             for i in range(len(hidden_dims))
         ]
-        self.scale = scale
         self.layers.append(eqx.nn.Linear(hidden_dims[-1], out_dim, key=keys[-1]))
 
-    def __call__(self, x: Float[Array, "2"]) -> Float[Array, "..."]:
-        x = x * self.scale
+    def __call__(self, x: Float[Array, "latent_dim"]) -> Float[Array, "..."]:
         for layer in self.layers[:-1]:
-            x = jax.nn.relu(layer(x))
+            x = jax.nn.gelu(layer(x))
         x = self.layers[-1](x)
         return jnp.clip(x, 0, 1)
 
 
-@eqx.filter_jit
-def train_step(
-    model: MLP,
-    optim: PyTree,
-    opt_state: PyTree,
-    coords: Float[Array, "*"],
-    pixels: Float[Array, "*"],
-) -> tuple:
-    """Performs a single training step."""
+class CombinedModel(eqx.Module):
+    """Needed to simplify the logic, and in case you desire to have some glue."""
 
-    def loss_fn(m: MLP) -> Float[Array, ""]:
-        preds = eqx.filter_vmap(m)(coords)
-        return jnp.mean((preds - pixels) ** 2)
+    latent_map: LatentMap
+    mlp: MLP
 
-    loss, grads = jax.value_and_grad(loss_fn)(model)
-    updates, opt_state = optim.update(grads, opt_state)
-    new_model = eqx.apply_updates(model, updates)
-    return new_model, opt_state, loss
-
-
-def sample_pixels(image: Float[Array, "h w *"], key: PRNGKeyArray, fraction: float = 0.25) -> tuple:
-    height, width = image.shape[:2]
-    num = max(1, int(height * width * fraction))
-    indices = jr.choice(key, height * width, shape=(num,), replace=False)
-    coords = jnp.stack(jnp.unravel_index(indices, (height, width)), axis=-1)
-    if image.ndim == 2:
-        pixels = image[coords[:, 1], coords[:, 0]].reshape(-1, 1)
-    else:
-        pixels = image[coords[:, 1], coords[:, 0], :]
-    return coords, pixels
+    def __call__(self, x: Int[Array, "2"]) -> Float[Array, "..."]:
+        latent = self.latent_map(x)
+        return self.mlp(latent)
 
 
 def compute_psnr(
@@ -86,23 +119,18 @@ def compute_psnr(
 
 
 def eval_image(
-    model: MLP, image: Float[Array, "h w *"], viz: str | None = None
+    model: CombinedModel, image: Float[Array, "w h *"], viz: str | None = None
 ) -> Float[Array, ""]:
-    """Generates the predicted image and computes PSNR."""
+    if len(image.shape) == 2:
+        image = image[..., None]
+    w, h, c = image.shape[:3]
 
-    if image.ndim == 2:
-        height, width = image.shape
-        out_dim = 1
-    else:
-        height, width, channels = image.shape
-        out_dim = channels
-
-    coords = jnp.stack(jnp.meshgrid(jnp.arange(width), jnp.arange(height)), axis=-1).reshape(-1, 2)
+    coords = make_mesh((w, h))
     preds = eqx.filter_vmap(model)(coords)
-    if out_dim == 1:
-        pred_image = preds.reshape(height, width)
-    else:
-        pred_image = preds.reshape(height, width, out_dim)
+    pred_image = preds.reshape(w, h) if c == 1 else preds.reshape(w, h, c)
+
+    image = image.squeeze()
+    pred_image = pred_image.squeeze()
 
     psnr = eqx.filter_jit(compute_psnr)(image, pred_image)
 
@@ -124,30 +152,85 @@ def eval_image(
         )
         combined_image.paste(true_image_pil, (0, 0))
         combined_image.paste(pred_image_pil, (true_image_pil.width, 0))
+        plt.figure()
+        plt.imshow(combined_image)
         with path.open("wb") as f:
             combined_image.save(f)
             time.sleep(0.05)
     return psnr
 
 
-def train_image(image: Float[Array, "w h *"], key: PRNGKeyArray, epochs: int = 1000) -> MLP:
-    """Trains an MLP model to represent a single image."""
-    if image.ndim == 2:
-        height, width = image.shape
-        out_dim = 1
-    else:
-        height, width, channels = image.shape
-        out_dim = channels
+def get_latent_points(key: PRNGKeyArray, image: Float[Array, "w h c"], fraction: float = 0.05):
+    w, h, c = image.shape
 
-    k1, k2 = jr.split(key)
-    model = MLP(2, [64, 64], out_dim, k1, scale=1.0 / float(max(width, height)))
+    sobel_x = jnp.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+    sobel_y = jnp.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+
+    grad_x = jnp.mean(
+        jax.vmap(lambda i: convolve(i, sobel_x, mode="same"), in_axes=-1)(image), axis=0
+    )
+    grad_y = jnp.mean(
+        jax.vmap(lambda i: convolve(i, sobel_y, mode="same"), in_axes=-1)(image), axis=0
+    )
+
+    grad_magnitude = jnp.sqrt(grad_x**2 + grad_y**2)
+    probs = grad_magnitude.flatten() / jnp.sum(grad_magnitude)
+    num_points = max(1, int(w * h * fraction))
+
+    indices = jr.choice(key, w * h, shape=(num_points,), p=probs)
+
+    coords = make_mesh((w, h))
+    return coords[indices]
+
+
+def train_step(
+    model: CombinedModel,
+    optim: PyTree,
+    opt_state: PyTree,
+    coords: Float[Array, "*"],
+    pixels: Float[Array, "*"],
+) -> tuple:
+    def loss_fn(m: CombinedModel) -> Float[Array, ""]:
+        preds = eqx.filter_vmap(m)(coords)
+        return jnp.mean((preds.ravel() - pixels.ravel()) ** 2)
+
+    loss, grads = eqx.filter_value_and_grad(loss_fn)(model)
+    updates, opt_state = optim.update(grads, opt_state)
+    new_model = eqx.apply_updates(model, updates)
+    return new_model, opt_state, loss
+
+
+def sample_pixels(image: Float[Array, "w h *"], key: PRNGKeyArray, fraction: float = 0.25) -> tuple:
+    w, h = image.shape[:2]
+    num = max(1, int(w * h * fraction))
+    indices = jr.choice(key, w * h, shape=(num,), replace=False)
+    coords = make_mesh((w, h))[indices]
+    pixels = image[coords[:, 1], coords[:, 0], :]
+    return coords, pixels
+
+
+def train_image(
+    image: Float[Array, "w h *"], key: PRNGKeyArray, epochs: int = 1000
+) -> CombinedModel:
+    """Trains an MLP model to represent a single image."""
+    if len(image.shape) == 2:
+        image = image[..., None]
+    w, h, c = image.shape
+
+    k1, k2, k3 = jr.split(key, 3)
+    mlp = MLP(32, [128], c, k1)
+
+    latent_points = get_latent_points(k2, image, fraction=0.05)
+    latent_map = LatentMap(k3, latent_points, image.shape)
+    model = CombinedModel(latent_map, mlp)
+
     optim = optax.adam(learning_rate=1e-4)
-    opt_state = optim.init(eqx.filter(model, eqx.is_array))
+    opt_state = optim.init(eqx.filter(model, eqx.is_inexact_array))
 
     def scan_fn(carry, _):
         model, opt_state, local_key = carry
         sample_key, subkey = jr.split(local_key)
-        batch_coords, batch_pixels = sample_pixels(image, subkey)
+        batch_coords, batch_pixels = sample_pixels(image, subkey, fraction=0.25)
         model, opt_state, loss = train_step(model, optim, opt_state, batch_coords, batch_pixels)
         return (model, opt_state, sample_key), loss
 
@@ -186,19 +269,21 @@ def main(argv):
     # Train on MNIST images
     logging.info("Training on first %d MNIST images...", num_train_mnist)
     for idx in range(min(num_train_mnist, mnist_images.shape[0])):
+        continue
         image = mnist_images[idx].reshape(28, 28)
-        key = jr.PRNGKey(idx)
+        key = jr.PRNGKey(idx + 1)
         logging.info("Training MNIST image %d", idx + 1)
         model = train_image(jnp.array(image), key, epochs=10000)
         psnr = eval_image(model, jnp.array(image), viz="mnist")
         logging.info("PSNR for MNIST image %d: %.2f", idx + 1, psnr)
+        plt.show()
 
     # Train on Imagenette images
     logging.info("Training on first %d Imagenette images...", num_train_imagenette)
     for idx, image in enumerate(imagenette_images):
         key = jr.PRNGKey(idx + 100)
         logging.info("Training Imagenette image %d", idx + 1)
-        model = train_image(jnp.array(image), key, epochs=1000)
+        model = train_image(jnp.array(image), key, epochs=1)
         psnr = eval_image(model, jnp.array(image), viz="imagenet")
         logging.info("PSNR for Imagenette image %d: %.2f", idx + 1, psnr)
         if idx == num_train_imagenette - 1:
