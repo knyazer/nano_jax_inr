e5e380105fdd53eece2ebddac8bf54c070b74ecc
Roman Knyazhitskiy <git@knyaz.tech>
2024-10-28 03:53:30 +0100

    feat: enhance image processing with new class

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import optax
 from absl import app, logging
 from jax.scipy.signal import convolve
-from jaxtyping import Array, Float, PRNGKeyArray, PyTree
+from jaxtyping import Array, Float, Int, PRNGKeyArray, PyTree
 from sklearn.datasets import fetch_openml
 
 from dataloader import load_imagenette_images, prepare_datasets, preprocess_mnist
@@ -20,32 +20,65 @@ from utils import make_mesh
 
 mpl.use("TkAgg")
 
+jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")  # noqa
+jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
 
-def get_latent_points(key: PRNGKeyArray, image: Float[Array, "w h c"], fraction: float = 0.05):
-    w, h, c = image.shape
+# jax.config.update("jax_log_compiles", True)
+
+MAX_DIM = 600
+
+
+class Image(eqx.Module):
+    data: Float[Array, "2048 2048 c"]
+    shape: Int[Array, "2"]
+    channels: int
+
+    def __init__(self, data, shape, channels):
+        self.data = data
+        self.shape = shape
+        self.channels = channels
+
+    def max_shape(self):
+        return (MAX_DIM, MAX_DIM)
+
+    def max_latents(self):
+        return int(self.max_shape()[0] * self.max_shape()[1] * 0.05)
+
+
+@jax.profiler.annotate_function
+@jax.named_scope("get_latent_points")
+def get_latent_points(key: PRNGKeyArray, image: Image, fraction: float = 0.05):
+    w, h, c = image.data.shape
 
     sobel_x = jnp.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
     sobel_y = jnp.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
 
     grad_x = jnp.sum(
-        jax.vmap(lambda i: convolve(i, sobel_x, mode="same"), in_axes=-1)(image), axis=0
+        jax.vmap(lambda i: convolve(i, sobel_x, mode="same"), in_axes=-1)(image.data), axis=0
     )
     grad_y = jnp.sum(
-        jax.vmap(lambda i: convolve(i, sobel_y, mode="same"), in_axes=-1)(image), axis=0
+        jax.vmap(lambda i: convolve(i, sobel_y, mode="same"), in_axes=-1)(image.data), axis=0
     )
 
-    grad_magnitude = jnp.sqrt(grad_x**2 + grad_y**2)
-    plt.figure()
-    plt.imshow(grad_magnitude)
+    grad_magnitude = jnp.nan_to_num(jnp.sqrt(grad_x**2 + grad_y**2))
 
-    num_points = max(1, int(w * h * fraction))
     x_grid, y_grid = jnp.meshgrid(jnp.arange(w), jnp.arange(h), indexing="ij")
     grid = jnp.stack([x_grid, y_grid], axis=-1).reshape(-1, 2)
 
-    indices = jr.choice(key, grid, shape=(num_points,), p=grad_magnitude.reshape(-1), replace=False)
+    num_latents = image.max_latents()
+    indices = jr.choice(
+        key, grid, shape=(num_latents,), p=grad_magnitude.reshape(-1), replace=False
+    )
+    num_points = image.shape[0] * image.shape[1] * fraction
+    indices = jnp.where(
+        (jnp.arange(num_latents) < num_points)[:, None].repeat(2, 1), indices, jnp.nan
+    )
     return indices
 
 
+@jax.profiler.annotate_function
+@jax.named_scope("train_step")
 def train_step(
     model: CombinedModel,
     optim: PyTree,
@@ -55,7 +88,7 @@ def train_step(
 ) -> tuple:
     def loss_fn(m: CombinedModel) -> Float[Array, ""]:
         preds = eqx.filter_vmap(m)(coords)
-        return jnp.mean((preds - pixels).ravel() ** 2)
+        return jnp.nanmean((preds - pixels).ravel() ** 2)
 
     loss, grads = eqx.filter_value_and_grad(loss_fn)(model)
     updates, opt_state = optim.update(grads, opt_state, eqx.filter(model, eqx.is_inexact_array))
@@ -63,37 +96,40 @@ def train_step(
     return new_model, opt_state, loss
 
 
-def sample_pixels(image: Float[Array, "w h *"], key: PRNGKeyArray, fraction: float = 0.25) -> tuple:
-    w, h = image.shape[:2]
-    num = max(1, int(w * h * fraction))
-    indices = jr.choice(key, w * h, shape=(num,), replace=False)
-    coords = make_mesh((w, h))[indices]
-    pixels = image[coords[:, 0], coords[:, 1], :]
-    return coords, pixels.reshape(-1, image.shape[-1])
+@eqx.filter_jit
+@jax.profiler.annotate_function
+@jax.named_scope("sample_pixels")
+def sample_pixels(
+    image: Image,
+    key: PRNGKeyArray,
+    fraction=0.25,
+) -> tuple:
+    w, h = image.shape
+    W, H = image.max_shape()  # noqa
+    indices = jr.choice(key, W * H, shape=(int(W * H * fraction),), replace=False)
+    coords = make_mesh((W, H))[indices]
+    pixels = image.data[coords[:, 0], coords[:, 1], :]
+    return coords, pixels
 
 
-def train_image(
-    image: Float[Array, "w h *"], key: PRNGKeyArray, epochs: int = 1000
-) -> CombinedModel:
+def train_image(image: Image, key: PRNGKeyArray, epochs: int = 1000) -> CombinedModel:
     """Trains an MLP model to represent a single image."""
-    if len(image.shape) == 2:
-        image = image[..., None]
-    w, h, c = image.shape
+    jax.debug.print("entering train image...")
+    image_data = image.data
+    w, h = image.shape  # 'channels' is separate cuz it must be static
+    c = image.channels
 
     k1, k2, k3, k4 = jr.split(key, 4)
 
     mlp = MLP(32, [32], c, k1)
     latent_points = get_latent_points(k2, image, fraction=0.05)
-    latent_map = LatentMap(k3, latent_points, image.shape)
-    model = CombinedModel(image, latent_map, mlp)
-
-    latent_points_viz = jnp.zeros((w, h, c))
-    latent_points_viz = latent_points_viz.at[latent_points[:, 0], latent_points[:, 1], 0].set(1)
-    plt.figure()
-    plt.imshow(latent_points_viz)
+    latent_map = LatentMap(k3, latent_points, image)
+    model = CombinedModel(image_data, latent_map, mlp)
 
     optim = optax.adam(learning_rate=1e-3)
     opt_state = optim.init(eqx.filter(model, eqx.is_inexact_array))
+
+    jax.debug.print("entering scan loop...")
 
     def scan_fn(carry, _):
         model, opt_state, local_key = carry
@@ -102,13 +138,9 @@ def train_image(
         model, opt_state, loss = train_step(model, optim, opt_state, batch_coords, batch_pixels)
         return (model, opt_state, sample_key), loss
 
-    for i in range(10):
-        (model, opt_state, _), losses = jax.lax.scan(
-            scan_fn, (model, opt_state, k4), None, length=epochs // 10
-        )
-        if i != 9:
-            psnr = eval_image(model, jnp.array(image))
-            logging.info("PSNR during training: %.2f", psnr)
+    (model, opt_state, _), losses = jax.lax.scan(
+        scan_fn, (model, opt_state, k4), None, length=epochs
+    )
 
     return model
 
@@ -134,7 +166,7 @@ def main(argv):
     logging.info("Imagenette dataset loaded with some (?) samples.")
 
     # Limit to first 10 images from each dataset for training
-    num_train_mnist = 1
+    num_train_mnist = 0
     num_train_imagenette = 1
 
     # Train on MNIST images
@@ -149,16 +181,29 @@ def main(argv):
 
     # Train on Imagenette images
     logging.info("Training on first %d Imagenette images...", num_train_imagenette)
-    imagenette_list = [next(imagenette_images) for _ in range(num_train_imagenette)]
-    for idx, image in enumerate(imagenette_list):
-        key = jr.PRNGKey(idx + 100)
-        logging.info("Training Imagenette image %d", idx + 1)
-        model = train_image(jnp.array(image), key, epochs=1000)
-        psnr = eval_image(model, jnp.array(image), viz="imagenet")
-        logging.info("PSNR for Imagenette image %d: %.2f", idx + 1, psnr)
-        jax.profiler.save_device_memory_profile("memory.prof")
-        if idx == num_train_imagenette - 1:
-            break
+
+    imagenette_list = []
+    for _ in range(num_train_imagenette):
+        image = next(imagenette_images)
+        w, h, c = image.shape
+        placeholder = jnp.full((MAX_DIM, MAX_DIM, c), jnp.nan)
+        placeholder = placeholder.at[:w, :h, :].set(image)
+        imagenette_list.append(Image(data=placeholder, shape=jnp.array([w, h]), channels=3))
+
+    imagenette_stacked_data = jnp.stack([image.data for image in imagenette_list])
+    imagenette_stacked_shape = jnp.stack([jnp.array(image.shape) for image in imagenette_list])
+
+    imagenette_stacked = Image(
+        data=imagenette_stacked_data, shape=imagenette_stacked_shape, channels=3
+    )
+
+    key = jr.PRNGKey(42)
+    fn = eqx.Partial(train_image, epochs=1000, key=key)
+    with jax.profiler.trace("/tmp/tensorboard"):
+        models = fn(imagenette_list[0])
+    jax.profiler.save_device_memory_profile("memory.prof")
+    exit()
+
     plt.show()
 
 
--- model.py
+++ model.py
@@ -1,8 +1,12 @@
+import time
+
 import equinox as eqx
 import jax
 import jax.numpy as jnp
 import jax.profiler
 import jax.random as jr
+import scipy.spatial
+from absl import logging
 from jaxtyping import Array, Float, Int, PRNGKeyArray
 
 from utils import make_mesh
@@ -14,28 +18,45 @@ class LatentMap(eqx.Module):
     positions: Int[Array, "n dim"]
     neighbor_map: Int[Array, "w h num_neighbors"]
     embeddings: Array
+    size: Int[Array, ""]
 
-    def __init__(self, key, positions, shape=(28, 28)):
+    def __init__(self, key, positions, image):
         positions = jnp.array(positions)
+        self.size = jnp.sum(jnp.any(jnp.isnan(positions), axis=-1))
+        coords = make_mesh(image.max_shape())
 
-        def compute_nearest_index(coord):
-            distances = jnp.linalg.norm(positions - coord, axis=1)
-            nearest_indices = jnp.argsort(distances)[:4]
-            return nearest_indices
+        def kd_neighbors_callback(positions, coords):
+            t = time.time()
+            # strip nans from positions
+            mask = jnp.logical_not(jnp.any(jnp.isnan(positions), axis=-1))
+            positions_stripped = positions[mask]
+            tree = scipy.spatial.KDTree(positions_stripped)
+            _, nearest_indices = tree.query(coords, k=4)
+            logging.info(f"spent {time.time() - t} seconds in kd callback")
+            return nearest_indices.astype(jnp.int32)
 
-        coords = make_mesh(shape)
-        nearest_indices = jax.lax.map(compute_nearest_index, coords)
+        shape = jnp.broadcast_shapes((image.max_shape()[0] * image.max_shape()[1], 4))
+        dtype = jnp.result_type(jnp.int32)
+        out_type = jax.ShapeDtypeStruct(shape, dtype)
+        nearest_indices = jax.pure_callback(
+            kd_neighbors_callback, out_type, positions, coords, vectorized=False
+        )
 
-        neighbor_map = jnp.zeros((shape[0], shape[1], 4))
+        neighbor_map = -jnp.ones((*image.max_shape(), 4))
         neighbor_map = neighbor_map.at[coords[:, 0], coords[:, 1]].set(nearest_indices)
 
-        self.neighbor_map = neighbor_map.astype(jnp.int16)
+        self.neighbor_map = neighbor_map.astype(jnp.int32)
         self.positions = positions
         self.embeddings = jr.normal(key, (len(positions), 32)) * 1e-3
 
+    @jax.named_scope("LatentMap::call")
+    @jax.profiler.annotate_function
     def __call__(self, position: Float[Array, "2"]):
         int_pos = jnp.floor(position).astype(jnp.int32)
         neighbors = self.neighbor_map[int_pos[0], int_pos[1]]
+        neighbors = eqx.error_if(
+            neighbors, neighbors.sum() < 0, "Undefined neighbors: index out of bounds."
+        )
         distances = jnp.linalg.norm(self.positions[neighbors] - int_pos, axis=1)
 
         if True:  # this branch is "joost's method" - which does not make any sense to me
@@ -71,6 +92,8 @@ class MLP(eqx.Module):
         ]
         self.layers.append(eqx.nn.Linear(hidden_dims[-1], out_dim, key=keys[-1]))
 
+    @jax.named_scope("mlp::call")
+    @jax.profiler.annotate_function
     def __call__(self, x: Float[Array, "latent_dim"]) -> Float[Array, "..."]:
         for layer in self.layers[:-1]:
             x = jax.nn.relu(layer(x))
@@ -79,7 +102,7 @@ class MLP(eqx.Module):
 
 
 class CombinedModel(eqx.Module):
-    """Needed to simplify the logic, and in case you desire to have some glue."""
+    """Glue + output normalization."""
 
     latent_map: LatentMap
     mlp: MLP
@@ -87,11 +110,13 @@ class CombinedModel(eqx.Module):
     std: Array
 
     def __init__(self, image, latent_map, mlp):
-        self.mu = jnp.mean(image, axis=(0, 1))
-        self.std = jnp.std(image, axis=(0, 1))
+        self.mu = jnp.nanmean(image, axis=(0, 1))
+        self.std = jnp.nanstd(image, axis=(0, 1))
         self.latent_map = latent_map
         self.mlp = mlp
 
+    @jax.named_scope("combined::call")
+    @jax.profiler.annotate_function
     def __call__(self, x: Int[Array, "2"]) -> Float[Array, "..."]:
         latent = self.latent_map(x)
         return jnp.clip(self.mlp(latent) * self.std + self.mu, 0, 1)
