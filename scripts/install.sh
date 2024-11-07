3c37fb2cf2134255aed655966e171f20cb7a429c
Roman Knyazhitskiy <git@knyaz.tech>
2024-11-07 10:04:36 +0100

    refactor: update requirements file for installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,7 @@ sudo apt install python3.11 python3.11-venv -y
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python3.11 get-pip.py
 
-python3.11 -m pip install -r nano_jax_inr/pyproject.toml -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+python3.11 -m pip install -r nano_jax_inr/tpu-requirements.txt -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 
 sudo mount -t tmpfs -o size=100G tmpfs ~/nano_jax_inr
 git clone https://github.com/knyazer/nano_jax_inr
