11a413c39ca5e7ba21e689a5cd206768e5e2b30b
Roman Knyazhitskiy <git@knyaz.tech>
2024-11-08 10:02:44 +0100

    refactor: change logging level in KDTree query

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,8 +3,10 @@ cd ~/nano_jax_inr/
 sudo apt install neofetch git build-essential -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install python3.11 python3.11-venv -y
+sudo snap install --classic go # for jax-smi
+
 
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python3.11 get-pip.py
 
-python3.11 -m pip install -r nano_jax_inr/tpu-requirements.txt -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+python3.11 -m pip install -r tpu-requirements.txt -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
