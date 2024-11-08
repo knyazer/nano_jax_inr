c743ce4370825525f447cc6b42c450b0f6b820ed
Roman Knyazhitskiy <git@knyaz.tech>
2024-11-08 09:55:48 +0100

    refactor: streamline installation script directory handling

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-cd ~
+cd ~/nano_jax_inr/
 
 sudo apt install neofetch git build-essential -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
@@ -8,8 +8,3 @@ curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python3.11 get-pip.py
 
 python3.11 -m pip install -r nano_jax_inr/tpu-requirements.txt -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-
-sudo mount -t tmpfs -o size=100G tmpfs ~/nano_jax_inr
-git clone https://github.com/knyazer/nano_jax_inr
-
-cd ~/nano_jax_inr/
